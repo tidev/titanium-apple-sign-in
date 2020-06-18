98e32200ab7db8b98dea1b64b6fb014081d0713f
Vikas Goyal <38314488+vikas-goyal-syncplicity@users.noreply.github.com>
2020-06-18 13:35:35 +0530

    test: improve unit test coverage

--- test/unit/specs/constants.spec.js
+++ test/unit/specs/constants.spec.js
@@ -1,18 +1,41 @@
-let AppleSignin;
+let AppleSignin = require('ti.applesignin');
 
 describe('ti.applesignin', () => {
-
 	it('can be required', () => {
-		AppleSignin = require('ti.applesignin');
-
 		expect(AppleSignin).toBeDefined();
 	});
 
-	// it('.apiName', () => {
-	// 	expect(AppleSignin.apiName).toBe('Ti.Applesignin');
-	// });
+	it('.moduleId', () => {
+		expect(AppleSignin.moduleId).toBe('ti.applesignin');
+	});
 
 	describe('constants', () => {
+		describe('AUTHORIZATION_SCOPE_*', () => {
+			it('AUTHORIZATION_SCOPE_EMAIL', () => {
+				expect(AppleSignin.AUTHORIZATION_SCOPE_EMAIL).toEqual('email');
+			});
+
+			it('AUTHORIZATION_SCOPE_FULLNAME', () => {
+				expect(AppleSignin.AUTHORIZATION_SCOPE_FULLNAME).toEqual('full_name');
+			});
+		});
+
+		describe('BUTTON_STYLE_*', () => {
+			it('BUTTON_STYLE_WHITE', () => {
+				expect(AppleSignin.BUTTON_STYLE_WHITE).toEqual(jasmine.any(Number));
+			});
+
+			it('BUTTON_STYLE_WHITE_OUTLINE', () => {
+				expect(AppleSignin.BUTTON_STYLE_WHITE_OUTLINE).toEqual(
+					jasmine.any(Number)
+				);
+			});
+
+			it('BUTTON_STYLE_BLACK', () => {
+				expect(AppleSignin.BUTTON_STYLE_BLACK).toEqual(jasmine.any(Number));
+			});
+		});
+
 		describe('BUTTON_TYPE_*', () => {
 			it('BUTTON_TYPE_DEFAULT', () => {
 				expect(AppleSignin.BUTTON_TYPE_DEFAULT).toEqual(jasmine.any(Number));
@@ -27,17 +50,41 @@ describe('ti.applesignin', () => {
 			});
 		});
 
-		describe('BUTTON_STYLE_*', () => {
-			it('BUTTON_STYLE_WHITE', () => {
-				expect(AppleSignin.BUTTON_STYLE_WHITE).toEqual(jasmine.any(Number));
+		describe('CREDENTIAL_STATE_*', () => {
+			it('CREDENTIAL_STATE_AUTHORIZED', () => {
+				expect(AppleSignin.CREDENTIAL_STATE_AUTHORIZED).toEqual(
+					jasmine.any(Number)
+				);
 			});
 
-			it('BUTTON_STYLE_WHITE_OUTLINE', () => {
-				expect(AppleSignin.BUTTON_STYLE_WHITE_OUTLINE).toEqual(jasmine.any(Number));
+			it('CREDENTIAL_STATE_NOT_FOUNFD', () => {
+				expect(AppleSignin.CREDENTIAL_STATE_NOT_FOUNFD).toEqual(undefined);
 			});
 
-			it('BUTTON_STYLE_BLACK', () => {
-				expect(AppleSignin.BUTTON_STYLE_BLACK).toEqual(jasmine.any(Number));
+			it('CREDENTIAL_STATE_REVOKED', () => {
+				expect(AppleSignin.CREDENTIAL_STATE_REVOKED).toEqual(
+					jasmine.any(Number)
+				);
+			});
+		});
+
+		describe('USER_DETECTION_STATUS_*', () => {
+			it('USER_DETECTION_STATUS_REAL', () => {
+				expect(AppleSignin.USER_DETECTION_STATUS_REAL).toEqual(
+					jasmine.any(Number)
+				);
+			});
+
+			it('USER_DETECTION_STATUS_UNKNOWN', () => {
+				expect(AppleSignin.USER_DETECTION_STATUS_UNKNOWN).toEqual(
+					jasmine.any(Number)
+				);
+			});
+
+			it('USER_DETECTION_STATUS_UNSUPPORTED', () => {
+				expect(AppleSignin.USER_DETECTION_STATUS_UNSUPPORTED).toEqual(
+					jasmine.any(Number)
+				);
 			});
 		});
 	});
--- test/unit/specs/methods.spec.js
+++ test/unit/specs/methods.spec.js
@@ -1,4 +1,9 @@
 const AppleSignin = require('ti.applesignin');
+var signInButton = AppleSignin.createLoginButton({
+	type: AppleSignin.BUTTON_TYPE_SIGNIN,
+	style: AppleSignin.BUTTON_STYLE_BLACK,
+	borderRadius: 10,
+});
 
 describe('ti.applesignin', () => {
 	describe('methods', () => {
@@ -6,9 +11,35 @@ describe('ti.applesignin', () => {
 			it('is a function', () => {
 				expect(AppleSignin.authorize).toEqual(jasmine.any(Function));
 			});
-			// TODO: verify return value is one of the RESULT_* constants!
 		});
 
-		// TODO: verify 'login' event
+		describe('#createLoginButton()', () => {
+			it('is a function', () => {
+				expect(AppleSignin.createLoginButton).toEqual(jasmine.any(Function));
+			});
+		});
+
+		describe('#getCredentialState()', () => {
+			it('is a function', () => {
+				expect(AppleSignin.getCredentialState).toEqual(jasmine.any(Function));
+			});
+		});
 	});
 });
+
+describe('ti.applesignin.LoginButton', () => {
+	describe('properties', () => {
+		it('should have valid borderRadius', () => {
+			expect(signInButton.borderRadius).toEqual(10);
+		});
+
+		it('should have valid type', () => {
+			expect(signInButton.type).toEqual(AppleSignin.BUTTON_TYPE_SIGNIN);
+		});
+
+		it('should have valid style', () => {
+			expect(signInButton.style).toEqual(AppleSignin.BUTTON_STYLE_BLACK);
+		});
+	});
+});
+
